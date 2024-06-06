baff3bb288df42056c74e5c4e33a90add5454e79
86158 <2410973613@qq.com>
2024-06-06 20:08:52 +0800

    初始

--- app/src/main/java/com/example/androidhomework/MainActivity.java
+++ app/src/main/java/com/example/androidhomework/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidhomework;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //普通用户
+    Button button1;
+    //超级用户
+    Button button2;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -20,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+    }
+
+    public void init() {
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        //普通用户
+        if (id == button1.getId()) {
+            Intent pri = new Intent(this, priuser.class);
+            startActivity(pri);
+            //超级用户
+        } else {
+            Intent sup = new Intent(this, supuser.class);
+            startActivity(sup);
+        }
     }
 }
